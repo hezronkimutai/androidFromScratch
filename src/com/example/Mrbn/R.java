6b0b5e7d5fae7a59a6b03fb4ac7e1ab75257aa9c
hezronkimutai <hezronchelimo.hc@gmail.com>
2021-01-02 14:43:40 +0300

    tried a different xml layout

--- src/com/example/Mrbn/R.java
+++ src/com/example/Mrbn/R.java
@@ -10,6 +10,10 @@ package com.example.Mrbn;
 public final class R {
     public static final class attr {
     }
+    public static final class id {
+        public static final int button=0x7f040001;
+        public static final int text=0x7f040000;
+    }
     public static final class layout {
         public static final int activity_main=0x7f020000;
     }
